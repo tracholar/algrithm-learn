bfc3ebc7682ad5c35f31af6538592af6f268290c
zuoyuan <zuoyuan@mail.ustc.edu.cn>
2014-07-11 13:27:24 +0800

    wuziqi search one step

--- ClassicalProblem/wuziqi.py
+++ ClassicalProblem/wuziqi.py
@@ -1,6 +1,8 @@
 # coding:gbk
 import numpy as np
 import sys
+import re
+import copy
 
 N = 15 # size of chess
 BLACK = 1
@@ -121,16 +123,121 @@ def IsWin(map):
 				last = ChangeColor(last)
 	return False
 	
-def SearchNextPos(map):
-	return 0,0
+def FindMaxV(map, pos, color):
+	map = copy.copy(map)
+	if map[pos[0],pos[1]] != EMPTY:
+		return 0
+	SetPos(map,pos,color)
+	maxV = [0,0,0,0]
+	for i in range(N):
+		xall = 0
+		cx = BLACK
+		xlast = BLACK
+		yall = 0
+		cy = BLACK
+		ylast = BLACK
+		
+		for j in range(N):
+			if map[i][j] == xlast:
+				xall += 1
+			elif map[i][j] == EMPTY:
+				maxV[0] = max(maxV[0], xall*5)
+				xall = 0
+			else:
+				xall = 1
+				xlast = ChangeColor(xlast)
+			
+			
+			if map[j][i] == ylast:
+				yall += 1
+			elif map[j][i] == EMPTY:
+				maxV[1] = max(maxV[1], yall*5)
+				yall = 0
+			else:
+				yall = 1
+				ylast = ChangeColor(ylast)
+	maxV[0] = max(maxV[0],xall*5)
+	maxV[1] = max(maxV[1],yall*5)
+			
+	for delta in range(5-N,N-5+1):
+		all = 0
+		last = BLACK
+		c = BLACK
+		
+		if delta<0:
+			si = 0
+			endi = N + delta
+		else:
+			si = delta
+			endi = N
+			
+		for i in range(si,endi):
+			j = i - delta
+			if map[i][j] == last:
+				all += 1
+			elif map[i][j] == EMPTY:
+				maxV[2] = max(maxV[2], all*5)
+				all = 0
+			else:
+				all = 1
+				last = ChangeColor(last)
+	maxV[2] = max(maxV[2], yall*5)
+	for sumij in range(4,2*N-5):
+		all = 0
+		last = BLACK
+		c = BLACK
+		
+		if sum<N:
+			si = 0
+			endi = sumij + 1
+		else:
+			si = sumij +1 - N
+			endi = N
+			
+		for i in range(si,endi):
+			j = sumij - i
+			if map[i][j] == last:
+				all += 1
+				
+			elif map[i][j] == EMPTY:
+				maxV[3] = max(maxV[3], all*5)
+				all = 0
+			else:
+				all = 1
+				last = ChangeColor(last)
+	maxV[3] = max(maxV[3], all*5) - 5
+	
+	return max(maxV)
+
+def FindMaxPos(map, color):
+	value = np.zeros((N,N))
+	maxV = 0
+	maxi = 0
+	maxj = 0
+	for i in range(N):
+		for j in range(N):
+			value[i][j] = min( i, N-1-i, j, N-1-j) + FindMaxV(map, (i,j), color)
+			if maxV < value[i][j]:
+				maxV = value[i][j]
+				maxi, maxj = i, j 
+	
+	return maxi, maxj
+			
+			
+def SearchNextPos(map, color):
+	return FindMaxPos(map, color)
 
 if __name__ == '__main__':
 	map = CreateMap()
-	map[7,7] = BLACK
-	map[7,8] = WHITE
+	print 'Begin!'
 	PrintMap(map)
 	while True:
 		line = sys.stdin.readline().strip()
+		if not re.match(r'^\d{1,2} \d{1,2}$', line):
+			if line in ['q','Q','quit']:
+				break
+			print 'Input formet error, please input with formet number + space + number'
+			continue
 		x, y = line.split(' ')
 		x = int(x)
 		y = int(y)
@@ -141,7 +248,7 @@ if __name__ == '__main__':
 				print 'You win!'
 				break
 				
-			pos = SearchNextPos(map)
+			pos = SearchNextPos(map, WHITE)
 			if not SetPos(map, pos, WHITE):
 				print 'Error: position (%d, %d) is not empty!' % (pos[0]+1,pos[1]+1)
 			PrintMap(map)
